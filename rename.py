dbe745e11d366b7c4aa3eafedf747f7d04d8f949
V Hoover <virgilhoover@gmail.com>
2022-04-17 03:58:27 -0600

    added another renaming function

--- rename.py
+++ rename.py
@@ -3,7 +3,7 @@ from os import rename, listdir, chdir, path
 from os.path import isfile
 
 
-def rename_files_with_search(loc):
+def rename_files_with_search(loc: str) -> None:
     ext = input('Please enter the extension your searching for: ')
     start = input('Please enter the first four characters of the name: ')
 
@@ -18,7 +18,7 @@ def rename_files_with_search(loc):
             print(file, 'has not been changed')
 
 
-def rename_with_search_pattern(loc):
+def rename_with_search_pattern(loc: str) -> None:
     old_term = input('Please enter the search term to remove: ')
     new_term = input('Please enter the new term to use: ')
     _counter = 0
@@ -30,7 +30,7 @@ def rename_with_search_pattern(loc):
         print("No file has been found")
 
 
-def rename_without_search(loc):
+def rename_without_search(loc: str) -> None:
     only_files = [item for item in listdir(loc) if isfile(item)]
     for file in only_files:
         ext = file[-4:]
@@ -40,7 +40,7 @@ def rename_without_search(loc):
         print('\n' + final_name, 'has been changed')
 
 
-def fix_file_name(loc):
+def fix_file_name(loc: str) -> None:
     only_files = [item for item in listdir(loc) if isfile(item)]
     for file in only_files:
         ext = file[-3:]
@@ -50,7 +50,7 @@ def fix_file_name(loc):
         print(final_name, 'has been changed\n')
 
 
-def rename_file():
+def rename_file() -> None:
     new_name = ''
     file = input('Please enter the filename to change: ')
     for item in file[:-4].split():
@@ -60,6 +60,17 @@ def rename_file():
     print(file, 'has been renamed to', new_name)
 
 
+def change_name_to_lower(loc: str) -> None:
+    only_files = [item for item in listdir(loc) if isfile(item)]
+    for file in only_files:
+        rename(file, file.lower())  # use upper() for the opposite goal
+
+    print("Done, all files to lower case")
+    for file, in listdir(loc):
+        print(file)
+    return None
+
+
 if __name__ == '__main__':
     p = input('Please enter the path to search: ')
     chdir(p)
@@ -67,7 +78,8 @@ if __name__ == '__main__':
                        'Press 2 to rename multiple files with a search\n'
                        'Press 3 to rename multiple files\n'
                        'Press 4 to fix file names\n'
-                       'Press 5 to define search term for file rename\n'))
+                       'Press 5 to define search term for file rename\n'
+                       'Press 6 to change to lower-case names\n'))
     if answer == 1:
         rename_file()
     elif answer == 2:
@@ -78,5 +90,7 @@ if __name__ == '__main__':
         fix_file_name(p)
     elif answer == 5:
         rename_with_search_pattern(p)
+    elif answer == 6:
+        change_name_to_lower(p)
     else:
         print('Invalid Choice!')
